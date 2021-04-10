476ab4cfec76e38637c75c347bdfd2d73d872437
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2021-04-09 22:14:25 -0700

    Loader complete.

--- builder/builder.cc
+++ builder/builder.cc
@@ -31,6 +31,31 @@ static void usage() {
     printf("-tload    if 'in' is a binary payload, use this address to load and jump to\n");
 }
 
+static uint32_t getU32(const uint8_t* ptr) {
+    uint32_t ret = 0;
+    ptr += 3;
+
+    ret |= *ptr--;
+    ret <<= 8;
+    ret |= *ptr--;
+    ret <<= 8;
+    ret |= *ptr--;
+    ret <<= 8;
+    ret |= *ptr--;
+    return ret;
+}
+
+static int16_t getHI(uint32_t v) {
+    int16_t lo = v & 0xffff;
+    int16_t hi = v >> 16;
+    return lo < 0 ? hi + 1 : hi;
+}
+
+static int16_t getLO(uint32_t v) {
+    int16_t ret = v & 0xffff;
+    return ret;
+}
+
 int main(int argc, char** argv) {
     banner();
 
@@ -54,7 +79,7 @@ int main(int argc, char** argv) {
     uint32_t vector;
     uint32_t oldAddr;
     uint32_t newAddr;
-    uint32_t tload;
+    uint32_t tload = 0;
 
     const char* argname = nullptr;
     try {
@@ -75,7 +100,7 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    std::array<uint8_t, 128 * 1024> out;
+    static std::array<uint8_t, 128 * 1024> out;
     memset(out.data(), 0, out.size());
     out[0x0000] = 'M';
     out[0x0001] = 'C';
@@ -117,7 +142,6 @@ int main(int argc, char** argv) {
     }
     out[0x00ff] = crc;
 
-    #if 0
     FILE* inFile = fopen(inName.value().c_str(), "rb");
     if (!inFile) {
         printf("Failed to open input file %s\n", inName.value().c_str());
@@ -132,9 +156,99 @@ int main(int argc, char** argv) {
     if (n != 1) {
         printf("Failed to read input file %s\n", inName.value().c_str());
         return -1;
-    } 
+    }
     fclose(inFile);
-    #endif
+
+    uint32_t tsize = in.size();
+    uint32_t gp = 0;
+    uint32_t pc = tload;
+    uint32_t sp = 0;
+
+    uint8_t* payloadPtr = in.data();
+
+    if (memcmp(in.data(), "PS-X EXE", 8) == 0) {
+        pc = getU32(in.data() + 0x10);
+        gp = getU32(in.data() + 0x14);
+        tload = getU32(in.data() + 0x18);
+        tsize = getU32(in.data() + 0x1c);
+        sp = getU32(in.data() + 0x30);
+        sp += getU32(in.data() + 0x34);
+        payloadPtr = in.data() + 2048;
+    } else {
+        if (!tloadStr.has_value()) {
+            printf("Raw binary and no tload argument.\n");
+            usage();
+            return -1;
+        }
+    }
+
+    if (sp == 0) sp = 0x801ffff0;
+    unsigned frames = (tsize + 127) >> 7;
+    constexpr unsigned availableFrames = ((128 * 1024) >> 7) - 17;
+    if (frames > availableFrames) {
+        printf("Payload binary too big. Maximum = %i bytes\n", availableFrames * 128);
+        return -1;
+    }
+    memcpy(out.data() + 17 * 128, payloadPtr, tsize);
+
+    uint32_t payload[] = {
+        // restoring old vector
+        Mips::Encoder::addiu(Mips::Encoder::Reg::V0, Mips::Encoder::Reg::R0, oldAddr),
+        Mips::Encoder::sw(Mips::Encoder::Reg::V0, vector, Mips::Encoder::Reg::R0),
+
+        // S0 = number of frames to load
+        Mips::Encoder::addiu(Mips::Encoder::Reg::S0, Mips::Encoder::Reg::R0, frames),
+        // S1 = destination address
+        Mips::Encoder::lui(Mips::Encoder::Reg::S1, getHI(tload)),
+        Mips::Encoder::addiu(Mips::Encoder::Reg::S1, Mips::Encoder::Reg::S1, getLO(tload)),
+        // S2 = frame counter (set to 0)
+        Mips::Encoder::addiu(Mips::Encoder::Reg::S2, Mips::Encoder::Reg::R0, 0),
+        // read_loop:
+        // A0 = deviceID (0)
+        Mips::Encoder::addiu(Mips::Encoder::Reg::A0, Mips::Encoder::Reg::R0, 0),
+        // A1 = frame counter + 17
+        Mips::Encoder::addiu(Mips::Encoder::Reg::A1, Mips::Encoder::Reg::S2, 17),
+        // A2 = current destination address pointer
+        Mips::Encoder::addiu(Mips::Encoder::Reg::A2, Mips::Encoder::Reg::S1, 0),
+        // increment frame counter
+        Mips::Encoder::addiu(Mips::Encoder::Reg::S2, Mips::Encoder::Reg::S2, 1),
+        // mcReadSector(0, frame, dest)
+        Mips::Encoder::jal(0xb0),
+        Mips::Encoder::addiu(Mips::Encoder::Reg::T1, Mips::Encoder::Reg::R0, 0x4f),
+        // mcWaitForStatus(0);
+        Mips::Encoder::addiu(Mips::Encoder::Reg::A0, Mips::Encoder::Reg::R0, 0),
+        Mips::Encoder::jal(0xb0),
+        Mips::Encoder::addiu(Mips::Encoder::Reg::T1, Mips::Encoder::Reg::R0, 0x5d),
+        // if we still haven't reached our frame counter, go back 40 bytes (aka read_loop)
+        Mips::Encoder::bne(Mips::Encoder::Reg::S0, Mips::Encoder::Reg::S2, -40),
+        // increment destination pointer by sizeof(frame) in the delay slot
+        Mips::Encoder::addiu(Mips::Encoder::Reg::S1, Mips::Encoder::Reg::S1, 128),
+
+        // flush cache
+        Mips::Encoder::jal(0xa0),
+        Mips::Encoder::addiu(Mips::Encoder::Reg::T1, Mips::Encoder::Reg::R0, 0x44),
+
+        // bootstrap
+        Mips::Encoder::lui(Mips::Encoder::Reg::V0, getHI(pc)),
+        Mips::Encoder::addiu(Mips::Encoder::Reg::V0, Mips::Encoder::Reg::V0, getLO(pc)),
+        Mips::Encoder::lui(Mips::Encoder::Reg::GP, getHI(gp)),
+        Mips::Encoder::addiu(Mips::Encoder::Reg::GP, Mips::Encoder::Reg::GP, getLO(gp)),
+        Mips::Encoder::lui(Mips::Encoder::Reg::SP, getHI(sp)),
+        Mips::Encoder::jr(Mips::Encoder::Reg::V0),
+        Mips::Encoder::addiu(Mips::Encoder::Reg::SP, Mips::Encoder::Reg::SP, getLO(sp)),
+    };
+    static_assert(sizeof(payload) < 128, "Payload too big");
+    unsigned o = 0;
+    for (auto p : payload) {
+        out[0x800 + o++] = p & 0xff;
+        p >>= 8;
+        out[0x800 + o++] = p & 0xff;
+        p >>= 8;
+        out[0x800 + o++] = p & 0xff;
+        p >>= 8;
+        out[0x800 + o++] = p & 0xff;
+        p >>= 8;
+    }
 
     FILE* outFile = fopen(outName.value().c_str(), "wb");
     if (!outFile) {
