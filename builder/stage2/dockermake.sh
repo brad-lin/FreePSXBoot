b7179972ce5b0bc7a6950ade540aed534fb3a3ef
Nicolas Noble <nicolasnoble@users.noreply.github.com>
2023-03-15 20:18:45 -0700

    Forgot one reference to dockerhub.

--- builder/stage2/dockermake.sh
+++ builder/stage2/dockermake.sh
@@ -6,5 +6,5 @@ cd $ROOT
 ROOT=$(pwd)
 cd $CWD
 
-docker pull grumpycoders/pcsx-redux-build:latest
+docker pull ghcr.io/grumpycoders/pcsx-redux-build:latest
 docker run --rm -i -w/project${CWD#$ROOT} -v "${ROOT}:/project" -u `id -u`:`id -g` ghcr.io/grumpycoders/pcsx-redux-build:latest make $@
