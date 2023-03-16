80859f20dad40418db31f1f6d1cda16dafcc5d3f
Nicolas Noble <nicolasnoble@users.noreply.github.com>
2023-03-15 20:15:45 -0700

    Switching from dockerhub to ghcr.io
    See https://github.com/docker/hub-feedback/issues/2314

--- builder/stage2/dockermake.sh
+++ builder/stage2/dockermake.sh
@@ -7,4 +7,4 @@ ROOT=$(pwd)
 cd $CWD
 
 docker pull grumpycoders/pcsx-redux-build:latest
-docker run --rm -i -w/project${CWD#$ROOT} -v "${ROOT}:/project" -u `id -u`:`id -g` grumpycoders/pcsx-redux-build:latest make $@
+docker run --rm -i -w/project${CWD#$ROOT} -v "${ROOT}:/project" -u `id -u`:`id -g` ghcr.io/grumpycoders/pcsx-redux-build:latest make $@
